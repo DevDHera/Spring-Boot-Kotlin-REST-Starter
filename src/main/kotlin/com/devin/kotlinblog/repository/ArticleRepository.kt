a174e392636a07671e8688be538ea7f5f427f03b
Devin Herath <devdhera@gmail.com>
2019-01-08 15:47:16 +0530

    Configured Article Repository

---
+++ src/main/kotlin/com/devin/kotlinblog/repository/ArticleRepository.kt
@@ -0,0 +1,8 @@
+package com.devin.kotlinblog.repository
+
+import com.devin.kotlinblog.model.Article
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ArticleRepository : JpaRepository<Article, Long>
